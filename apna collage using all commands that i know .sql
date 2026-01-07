502f01eca222e1a62c95d1d1f9e1df1f87a4c2fe
Sam <workforambrose@gmail.com>
2026-01-07 22:26:51 +0530

    Add files via upload

---
+++ apna collage using all commands that i know .sql
@@ -0,0 +1,16 @@
+-- create database if not exists collage1;
+use collage1;
+--  create table student (roll_no int primary key , name varchar(50) , marks int not null, grade varchar(1), city varchar (50) not null);
+ -- INSERT INTO student(roll_no, name, marks, grade, city)
+-- VALUES
+-- (101, "anil", 78, "C", "Pune"), (102, "bhumika", 93, "A", "Mumbai"), (103, "chetan", 85, "B", "Mumbai"), (104, "dhruv", 96, "A", "Delhi"), (105, "emanuel", 12, "F", "Delhi"), (106, "farah", 82, "B", "Delhi");
+
+
+
+select city, count(name) 
+from student
+where grade = "A"
+group by (city)
+having max(marks)>=90
+order by (city) desc
+;
